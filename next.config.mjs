9ab49980003ca8c73d5d4429819ff13acdbff3ad
Filipe Bacof <filipebacof@gmail.com>
2024-08-05 16:53:59 -0300

    feat: random player

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig =  {
+  images: {
+    domains: [
+      "encrypted-tbn0.gstatic.com",
+      "upload.wikimedia.org",
+      "www.footballdatabase.eu",
+      "fifpro.org",
+      "cloudfront-eu-central-1.images.arcpublishing.com",
+      "pbs.twimg.com",
+      "publish-p47754-e237306.adobeaemcloud.com",
+      "icdn.football-espana.net",
+      "terceirotempo.uol.com.br",
+      "img.asmedia.epimg.net",
+      "tmssl.akamaized.net",
+      "static.independent.co.uk",
+      "oneftbl-cms.imgix.net",
+      "s2-ge.glbimg.com",
+      "www.cnnbrasil.com.br",
+      "www.ogol.com.br",
+      "s2-oglobo.glbimg.com",
+      "www.portalin.com.br"
+    ],
+  },
+}
 
 export default nextConfig;
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,20 +1,38 @@
+"use client";
+
+import { players } from "@/utils/players";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Result() {
+  const [randomIndex, setRandomIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    const getRandomIndex = () => Math.floor(Math.random() * players.length);
+
+    setRandomIndex(getRandomIndex());
+  }, []);
+
   return (
     <main className="flex flex-col items-center my-8 gap-4">
-      <Image
-        className="w-60 h-auto rounded-md"
-        src="/Luka-Modric.jpg"
-        alt="Real Madrid Team"
-        width={559}
-        height={713}
-        priority
-      />
-      <h1 className="text-center text-lg font-semibold">
-        Você tirou: Luka Modric
-      </h1>
+      {randomIndex !== null ? (
+        <>
+          <Image
+            className="w-60 h-auto rounded-md"
+            src={players[randomIndex].picture}
+            alt={`Imagem de ${players[randomIndex].nome}`}
+            width={1000}
+            height={1000}
+            priority
+          />
+          <h1 className="text-center text-lg font-semibold">
+            Você tirou: {players[randomIndex].nome}
+          </h1>
+        </>
+      ) : (
+        <p>Carregando...</p>
+      )}
       <Link
         href="/"
         className="bg-emerald-500 mt-4 rounded font-semibold flex justify-center items-center px-4 py-2 text-zinc-800 h-10 hover:bg-emerald-600"
